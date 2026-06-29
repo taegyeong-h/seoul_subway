aa96c2ac7a5dd45d206759032ed735aab60ba5e7
taegyeong-h <a01026676497@gmail.com>
2026-06-29 17:16:20 +0900

    tset

--- src/common/db_connectors.py
+++ src/common/db_connectors.py
@@ -1,8 +1,21 @@
-import polars from pl
+
 from abc import ABC, abstractmethod
 
 
 
 class BaseDBConnector(ABC):
     @abstractmethod
-    def get_connection_url(self) -> str:ss
+    def get_connection_url(self) -> str:
+        pass
+
+
+class PostgresDBConnector(BaseDBConnector):
+    def __init__(self, db_config: dict):
+        self.user = db_config["user"]
+        self.password = db_config["password"]
+        self.host = db_config["host"]
+        self.port = db_config["port"]
+        self.database_name = db_config["database_name"]
+
+    def get_connection_url(self) -> str:
+        return f"postgresql://{self.user}:{self.password}@{self.host}:{self.port}/{self.database_name}"
